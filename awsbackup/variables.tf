009943a6e2a6a78e3ccf0d34e6570378a58c3ef8
jonathanpcruz <54188177+jonathanpcruz@users.noreply.github.com>
2022-07-05 14:17:04 -0300

    Create variables.tf

---
+++ awsbackup/variables.tf
@@ -0,0 +1,44 @@
+# Global parameters
+variable "aws_assume_role" {
+}
+
+variable "resource_prefix" {
+  type        = string
+  description = "Prefix name for AWS Backup resources"
+}
+
+variable "copy_account_id" {
+  type        = string
+  description = "Target AWS account Id for cross-account copies"
+}
+
+variable "organization_id" {
+  type        = string
+  description = "AWS Organization ID"
+}
+
+variable "tag_key_scp_protection" {
+  type        = string
+  description = "Tag key for AWS Organizations protected resources with SCPs"
+}
+
+variable "tag_value_scp_protection" {
+  type        = string
+  description = "Tag value for AWS Organizations protected resources with SCPs"
+}
+
+# Parameters for AWS Backup notification features
+variable "install_notifications" {
+  type        = bool
+  description = "Do you want to deploy central backup notification features?"
+}
+
+variable "main_account_id" {
+  type        = string
+  description = "AWS account ID of AWS Organization main account"
+}
+
+variable "main_region" {
+  type        = string
+  description = "Second AWS Region where resources must be protected - They will be copied into region_A on copy_account_id"
+}
